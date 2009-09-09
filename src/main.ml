e2b967f81ae5a6a49b2c7eca3fd7c19c63c335e5
Sebastien Mondet <seb@smtmb.(none)>
2009-09-09 22:45:46 +0200

    Added BibTeX output (as -bibtex <file>).

--- src/main.ml
+++ src/main.ml
@@ -46,8 +46,68 @@ module Biblio = struct
     let string_of_set set =
         Sexplib.Sexp.to_string (sexp_of_set set)
 
+    let find_field field (entry:entry) = (
+        let f  = List.Exceptionless.find in
+        match field with
+        | `id        -> (f (function `id       v -> true | _ -> false) entry)
+        | `authors   -> (f (function `authors  v -> true | _ -> false) entry)
+        | `title     -> (f (function `title    v -> true | _ -> false) entry)
+        | `how       -> (f (function `how      v -> true | _ -> false) entry)
+        | `date      -> (f (function `date     v -> true | _ -> false) entry)
+        | `year      -> (f (function `year     v -> true | _ -> false) entry)
+        | `url       -> (f (function `url      v -> true | _ -> false) entry)
+        | `pdfurl    -> (f (function `pdfurl   v -> true | _ -> false) entry)
+        | `image     -> (f (function `image    v -> true | _ -> false) entry)
+        | `comments  -> (f (function `comments v -> true | _ -> false) entry)
+        | `bibtex    -> (f (function `bibtex   v -> true | _ -> false) entry)
+        | `note      -> (f (function `note     v -> true | _ -> false) entry)
+        | `abstract  -> (f (function `abstract v -> true | _ -> false) entry)
+        | `doi       -> (f (function `doi      v -> true | _ -> false) entry)
+        | `citation  -> (f (function `citation v -> true | _ -> false) entry)
+    )
 end
 
+module BibTeX = struct
+
+    
+    let authors_string authors = (
+        String.concat " and "
+            (List.map (fun (last, first) ->
+                sprintf p"%s, %s" last first) authors)
+    )
+
+    let str set = (
+        let field_or_empty fi entry = 
+            match Biblio.find_field fi entry with
+            | Some (`authors al) -> authors_string al
+            | Some (`title tit) -> tit
+            | Some (`id id) -> id
+            | Some (`how how) -> how
+            | Some (`year y) -> string_of_int y
+            | Some (`note n) -> n
+            | _ -> ""
+        in
+        String.concat "\n\n"
+            (List.map (fun entry ->
+                match Biblio.find_field `bibtex entry with
+                | Some (`bibtex b) -> b
+                | _ ->
+                    sprintf p"@misc {%s,\n\
+                        \    author = {%s},\n\
+                        \    title = {%s},\n\
+                        \    howpublished = {%s},\n\
+                        \    year = {%s},\n\
+                        \    note = {%s}\n\
+                    }\n"
+                    (field_or_empty `id entry)
+                    (field_or_empty `authors entry)
+                    (field_or_empty `title entry)
+                    (field_or_empty `how entry)
+                    (field_or_empty `year entry)
+                    (field_or_empty `note entry)
+            ) set)
+    )
+end
 
 let testminimal () = (
     let sexp_set =
@@ -66,6 +126,7 @@ let () = (
     );
     let do_validate = ref false in
     let read_stdin = ref false in
+    let bibtex = ref "" in
     let usage = "sebib [OPTIONS] file1.sebib file2.sebib ..." in
     let commands = [
         Arg.command
@@ -76,16 +137,21 @@ let () = (
             ~doc:"Also use stdin as input" 
             "-stdin"
             (Arg.Set read_stdin);
+        Arg.command
+            ~doc:"Output a BibTeX file (- for stdout)" 
+            "-bibtex"
+            (Arg.Set_string bibtex);
     ] in
     let files = Arg.handle ~usage commands in
 
     let bibliography_str =
+        let strs_from_files =
+            List.map (fun file -> File.with_file_in file IO.read_all) files in
         String.concat "" 
-            ((IO.read_all stdin) ::
-                (List.map (fun file ->
-                    File.with_file_in file (fun i -> IO.read_all i)
-                ) files))
-    in
+            (if !read_stdin then 
+                ((IO.read_all stdin) :: strs_from_files)
+            else strs_from_files) in
+
     let biblio = Biblio.set_of_string bibliography_str in
 
     if !do_validate then (
@@ -96,9 +162,15 @@ let () = (
                 (Biblio.string_of_set l);
             exit 2;
     );
+    begin match !bibtex with
+    | "" -> ();
+    | "-" -> printf p"%s\n" (BibTeX.str biblio);
+    | f -> 
+        File.with_file_out f (fun o -> fprintf o p"%s" (BibTeX.str biblio));
+    end;
 
 
-    printf p"END\n%!";
+    (* printf p"END\n%!"; *)
 
 
 )

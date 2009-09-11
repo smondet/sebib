d2ad06efa3adf1a6cbcf05ac4ebfc8c1119c9cae
Sebastien Mondet <seb@smtmb.(none)>
2009-09-11 11:53:48 +0200

    Added sanitization of BibTeX output.
    This will be a never-ending process as UTF8 is very far from BibTeX
    considerations...

--- src/main.ml
+++ src/main.ml
@@ -157,29 +157,158 @@ end
 
 module BibTeX = struct
 
+    (* 
+     This function will always be work-in-progress...
+
+     Special Thanks to Vim (www.vim.org) for easing this editing
+
+     The Comprehensive LaTeX Symbol List
+     www.stat.berkeley.edu/~sourav/symbols-a4.pdf 
+     *)
+    let latexify = function
+
+        | "\\" -> "\\textbackslash{}"
+
+        | "$" -> "\\$"
+        | "%" -> "\\%"
+        | "_" -> "\\_"
+        | "}" -> "\\}"
+        | "&" -> "\\&"
+        | "#" -> "\\#"
+        | "{" -> "\\{"
+        | "^" -> "\\^{}"
+
+        | "á" -> "\\'a"
+        | "é" -> "\\'e"
+        | "í" -> "\\'i"
+        | "ó" -> "\\'o"
+        | "ú" -> "\\'u"
+        | "ý" -> "\\'y"
+
+        | "Á" -> "\\'A"
+        | "É" -> "\\'E"
+        | "Í" -> "\\'I"
+        | "Ó" -> "\\'O"
+        | "Ú" -> "\\'U"
+        | "Ý" -> "\\'Y"
+
+        | "à" -> "\\`a"
+        | "è" -> "\\`e"
+        | "ì" -> "\\`i"
+        | "ò" -> "\\`o"
+        | "ù" -> "\\`u"
+        
+        | "À" -> "\\`A"
+        | "È" -> "\\`E"
+        | "Ì" -> "\\`I"
+        | "Ò" -> "\\`O"
+        | "Ù" -> "\\`U"
+
+        | "â" -> "\\^a"
+        | "ê" -> "\\^e"
+        | "î" -> "\\^i"
+        | "ô" -> "\\^o"
+        | "û" -> "\\^u"
+
+        | "Â" -> "\\^A"
+        | "Ê" -> "\\^E"
+        | "Î" -> "\\^I"
+        | "Ô" -> "\\^O"
+        | "Û" -> "\\^U"
+
+        | "ä" -> "\\\"{a}"
+        | "ë" -> "\\\"{e}"
+        | "ï" -> "\\\"{i}"
+        | "ö" -> "\\\"{o}"
+        | "ü" -> "\\\"{u}"
+        | "ÿ" -> "\\\"{y}"
+
+        | "Ä" -> "\\\"{A}"
+        | "Ë" -> "\\\"{E}"
+        | "Ï" -> "\\\"{I}"
+        | "Ö" -> "\\\"{O}"
+        | "Ü" -> "\\\"{U}"
+        | "Ÿ" -> "\\\"{Y}"
+
+        | "ā" -> "\\={a}"
+        | "ē" -> "\\={e}"
+        | "ī" -> "\\={i}"
+        | "ū" -> "\\={u}"
+
+        | "ş" -> "\\c{s}"
+        | "ç" -> "\\c{c}"
+        | "ţ" -> "\\c{t}"
+
+        | "ñ" -> "\\~{n}"
+        | "Ñ" -> "\\~{N}"
+
+
+        | "ø" -> "\\o{}"
+        | "ß" -> "\\ss{}"
+        | "Ø" -> "\\O{}"
+        | "æ" -> "\\ae{}"
+        | "œ" -> "\\oe{}"
+        | "Æ" -> "\\AE{}"
+        | "Œ" -> "\\OE{}"
+
+        | "«" -> "\\guillemotleft{}"
+        | "‹" -> "\\guilsinglleft{}"
+        | "„" -> "\\quotedblbase{}"
+        | "\"" -> "\\textquotedbl{}"
+        | "»" -> "\\guillemotright{}"
+        | "›" -> "\\guilsinglright{}"
+        | "‚" -> "\\quotesinglbase{}"
+
+        | "£" -> "\\pounds{}"
+        | "€" -> "\\euro{}"
+        | "¥" -> "\\textyen{}"
+        | "₩" -> "\\textwon{}"
+
+        | "©" -> "\\textcopyright{}"
+        | "†" -> "\\textdagger{}"
+        | "‘" -> "\\textquoteleft{}"
+        | "’" -> "\\textquoteright{}"
+        | "‡" -> "\\textdaggerdbl{}"
+        | "®" -> "\\textregistered{}"
+
+        | s -> s
+
+    let sanitize_latex str = (
+        let rope = Rope.of_string str in
+        let ascii_buff = Buffer.create 42 in
+        let u2s u = Rope.to_string (Rope.of_uchar u) in
+
+        Rope.iter (fun u ->
+            Buffer.add_string ascii_buff (latexify (u2s u));
+        ) rope;
+        Buffer.contents ascii_buff
+    )
 
     let str set = (
-        let field_or_empty fi entry = 
-            Biblio.field_or_empty ~authors_style:`bibtex fi entry in
-        (* TODO: sanitize for (Bib)TeX *)
+        let field fi entry = 
+            Biblio.field_or_empty ~authors_style:`bibtex fi entry
+                |> sanitize_latex in
+        let sanitize_title str =
+            let rgx = Str.regexp "\\([^\\]\\)\\([A-Z]+\\)" in
+            Str.global_replace rgx "\\1{\\2}" str in
         String.concat "\n\n"
             (List.map (fun entry ->
                 match Biblio.find_field `bibtex entry with
                 | Some (`bibtex b) -> b
                 | _ ->
-                    sprintf p"@misc {%s,\n\
+                    sprintf p"@misc{%s,\n\
                         \    author = {%s},\n\
                         \    title = {%s},\n\
                         \    howpublished = {%s},\n\
                         \    year = {%s},\n\
                         \    note = {%s}\n\
                     }\n"
-                    (field_or_empty `id entry)
-                    (field_or_empty `authors entry)
-                    (field_or_empty `title entry)
-                    (field_or_empty `how entry)
-                    (field_or_empty `year entry)
-                    (field_or_empty `note entry)
+                    (field `id entry)
+                    (field `authors entry)
+                    (sanitize_title (field `title entry))
+                    (field `how entry)
+                    (field `year entry)
+                    (field `note entry)
             ) set)
     )
 end

f2043053368615293ace6e7d67358e199a5bd8b5
Sebastien Mondet <smondet@dmms-lab22.ifi.uio.no>
2010-01-13 18:23:02 +0100

    Sebib_std: Added convience renamed modules.

--- src/lib/sebib_std.ml
+++ src/lib/sebib_std.ml
@@ -13,3 +13,18 @@ module Ls = struct
                       
 
 end
+
+module Str = struct
+  (* This module name voluntarily forbids the Str module of the str
+     library *)
+  include ExtString
+  include StringLabels
+end
+
+module Ht = struct
+  include ExtHashtbl.Hashtbl
+  let find_opt ht key =
+    try Some (find ht key) with Not_found -> None
+  let value_list ht = Ls.of_enum (values ht)
+end
+

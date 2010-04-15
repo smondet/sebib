153a3cab238aa0bd639a97439375c59b9ff6e076
Sebastien Mondet <smondet@dmms-lab22.ifi.uio.no>
2010-04-15 11:09:41 +0200

    all: Added an MIT license to all the sources.

--- src/lib/sebib.ml
+++ src/lib/sebib.ml
@@ -1,3 +1,31 @@
+(******************************************************************************)
+(*      Copyright (c) 2009, 2010, Sebastien MONDET                            *)
+(*                                                                            *)
+(*      Permission is hereby granted, free of charge, to any person           *)
+(*      obtaining a copy of this software and associated documentation        *)
+(*      files (the "Software"), to deal in the Software without               *)
+(*      restriction, including without limitation the rights to use,          *)
+(*      copy, modify, merge, publish, distribute, sublicense, and/or sell     *)
+(*      copies of the Software, and to permit persons to whom the             *)
+(*      Software is furnished to do so, subject to the following              *)
+(*      conditions:                                                           *)
+(*                                                                            *)
+(*      The above copyright notice and this permission notice shall be        *)
+(*      included in all copies or substantial portions of the Software.       *)
+(*                                                                            *)
+(*      THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,       *)
+(*      EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES       *)
+(*      OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND              *)
+(*      NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT           *)
+(*      HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,          *)
+(*      WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING          *)
+(*      FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR         *)
+(*      OTHER DEALINGS IN THE SOFTWARE.                                       *)
+(******************************************************************************)
+
+
+
+
 (** The toplevel {i Sebib} module. *)
 
 
@@ -12,6 +40,32 @@ module Info = struct
 
   (** The version string with the name of the library. *)
   let version_string = sprintf "SeBib v. %s" version
+
+  (** The license string. *)  
+  let license = "\
+Copyright (c) 2008, 2009, 2010 Sebastien MONDET\n\
+\n\
+Permission is hereby granted, free of charge, to any person        \n\
+obtaining a copy of this software and associated documentation     \n\
+files (the \"Software\"), to deal in the Software without            \n\
+restriction, including without limitation the rights to use,       \n\
+copy, modify, merge, publish, distribute, sublicense, and/or sell  \n\
+copies of the Software, and to permit persons to whom the          \n\
+Software is furnished to do so, subject to the following           \n\
+conditions:                                                        \n\
+\n\
+The above copyright notice and this permission notice shall be     \n\
+included in all copies or substantial portions of the Software.    \n\
+\n\
+THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND,    \n\
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES    \n\
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND           \n\
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT        \n\
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,       \n\
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING       \n\
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR      \n\
+OTHER DEALINGS IN THE SOFTWARE.                                    \n\
+"
 
 end
 
